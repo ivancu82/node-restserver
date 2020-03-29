60bd26d064c92e05e25c1da9bf68720c0d82935c
ivancu82 <ivan.cuenca82@gmail.com>
2020-03-29 17:19:45 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Restserver
+
+```
+    npm install
+```
---
+++ server/server.js
@@ -0,0 +1,49 @@
+//require = ('./config/config');
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/usuario', (req, res) => {
+    res.json('get Usuario');
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete Usuario');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
+
+
+
