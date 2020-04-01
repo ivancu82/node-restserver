f22978ba907b0b79b9112286274e999880f571b4
ivancu82 <ivan.cuenca82@gmail.com>
2020-04-02 00:13:59 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ivancj:<WEmiB2B1boW3NbSR>@cluster0-7qqox.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
