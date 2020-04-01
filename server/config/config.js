74db40877001e3dd2a0081809de1dae2c28c84d2
ivancu82 <ivan.cuenca82@gmail.com>
2020-04-01 16:04:39 +0200

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-
 //===============
 // Puerto
 //===============
 process.env.PORT = process.env.PORT || 3000;
+
+//===============
+// Entorno
+//===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===============
+// Base de datos
+//===============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://ivancj:<WEmiB2B1boW3NbSR>@cluster0-7qqox.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
+
+
--- server/server.js
+++ server/server.js
@@ -1,49 +1,37 @@
-//require = ('./config/config');
+require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario');
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB,
+        { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},
+        (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
 
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario');
-});
-
+/*
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
+ */
+
+
+
+
 
 
 
